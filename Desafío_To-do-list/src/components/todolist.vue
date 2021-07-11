f64b47a7dda6ae1550fe7efea0a9d1b47452e5b3
Constanza Barrios <co.fuenzalida@gmail.com>
2021-07-11 10:30:14 -0400

    Borré un methods del todolist.vue que no estaba usando y le agregué estilos generales para que pareciera una lista

--- Desafío_To-do-list/src/components/todolist.vue
+++ Desafío_To-do-list/src/components/todolist.vue
@@ -1,65 +1,61 @@
 <template>
-    <div class="container">
-        <div class="row">
-        <h2>Lista de quehaceres</h2>
-        <div>
+    <div class="col s12 m6">
+        <div class="card medium yellow lighten-2">
+            <div class="card-content">
+                <span class="card-title">Quehaceres</span>
             <!-- INPUT -->
-            <div class="form">
-            <!-- v-model creates a two-way binding on a form input element or a component.-->
-            <input v-model="task" type="text" placerholder="Ingresar texto aquí" class="form-control">
-            <!-- otro tipo de v-on:click -->
-            <button @click="submitTask" class="btn waves-effect waves-light" type="submit" name="action">Anotar
-                <i class="fas fa-seedling"></i>
-            </button>
+                <div class="form">
+                    <!-- v-model creates a two-way binding on a form input element or a component.-->
+                    <!-- @keyup.enter sirve para que funcione el enter y reemplace al @click -->
+                    <input v-model="task" type="text" class="form-control" @keyup.enter="submitTask">
+                    <!-- otro tipo de v-on:click -->
+                    <button @click="submitTask" class="btn waves-effect yellow lighten-5 blue-text text-darken-2" type="submit" name="action">Anotar
+                        <i class="fas fa-seedling"></i>
+                    </button>
+                </div>
+
+                <!-- TABLA -->
+                <table class="highlight table">
+                    <thead>
+                    <tr>
+                        <th>Tarea</th>
+                        <th>Estado</th>
+                        <th></th>
+                        <th></th>
+
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                        <!-- Tenemos que especificar de dónde viene el data -->
+                        <!-- Cuando se usa un v-for, se recomienda usar un :key -->
+                    <tr v-for="(task, index) in tasks" :key="index">
+                        <!-- if ternario // en una línea -->
+                        <!-- <td :class="`${task.checked?'crossed':''}`">{{task.name}}</td> -->
+                        <!-- if con && que me va a servir en el futuro. Si checked es verdadero, me
+                        va a imprimir crossed. -->
+                        <td :class="`${task.checked&&'crossed'}`">{{task.name}}</td>
+                        <td>
+                            <label>
+                                <input id="checkbox" type="checkbox" v-model="task.checked" />
+                                <span ></span>
+                                <label for="checkbox"></label>
+                            </label>
+                        </td>
+                        <td>
+                            <div>
+                                <span v-on:click="deleteTask(index)" class="fa fa-trash"></span>
+                            </div>
+                        </td>
+                        
+                    </tr>
+                    </tbody>
+                </table>       
             </div>
-            <!-- TABLA -->
-            <table class="highlight table">
-                <thead>
-                <tr>
-                    <th>Tarea</th>
-                    <th>Estado</th>
-                    <th></th>
-                    <th></th>
-
-                </tr>
-                </thead>
-
-                <tbody>
-                    <!-- Tenemos que especificar de dónde viene el data -->
-                    <!-- Cuando se usa un v-for, se recomienda usar un :key -->
-                <tr v-for="(task, index) in tasks" :key="index">
-                    <!-- if ternario // en una línea -->
-                    <!-- <td :class="`${task.checked?'crossed':''}`">{{task.name}}</td> -->
-                    <!-- if con && que me va a servir en el futuro. Si checked es verdadero, me
-                    va a imprimir crossed. -->
-                    <td :class="`${task.checked&&'crossed'}`">{{task.name}}</td>
-                    <td>
-                        <label>
-                            <input id="checkbox" type="checkbox" v-model="task.checked" />
-                            <span ></span>
-                            <label for="checkbox"></label>
-                        </label>
-                    </td>
-                    <td>
-                        <div>
-                            <span v-on:click="deleteTask(index)" class="fa fa-trash"></span>
-                        </div>
-                    </td>
-                    
-                </tr>
-
-
-                </tbody>
-            </table>
-            
-        </div>
-
         </div>
     </div>
 </template>
 <script>
-
-
     export default{
         name: 'TodoList',
         props: {
@@ -74,6 +70,7 @@
         },
         methods: {
             submitTask(){
+                // por si no hay nada en el texto
                 if(this.task.length === 0) return;
                 this.tasks.push({
                     name: this.task,
@@ -84,24 +81,14 @@
             },
             deleteTask(index){
                 this.tasks.splice(index, 1);
-            },
-            hideTask(){
-                this.mostrar = !this.mostrar
             }
         }    
-
     };
-    
-
-
-
-
 </script>
 
 <style scoped>
     .form {
         display:flex;
-        width: 400px;
     }
     .crossed {
         text-decoration: line-through;
